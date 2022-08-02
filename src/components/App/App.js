0bfba37946231bb4bfc1fc273a464881b69160cb
Aleksandr Demidenko <sashatro@mail.ru>
2022-08-02 20:20:18 +0300

    Добавить шрифты и normolize

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,7 +10,7 @@ import Register from "./Register/Register";
 
 function App() {
   return (
-      <div className="App">
+      <div className="page">
         <Switch>
           <Route exact path="/">
             <Main />
