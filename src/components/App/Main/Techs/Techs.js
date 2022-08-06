ddf3baf1298ddf0fa22761f29f175e69310bdb4b
Aleksandr Demidenko <sashatro@mail.ru>
2022-08-06 12:47:05 +0300

    Add Tech component for all devices

--- src/components/App/Main/Techs/Techs.js
+++ src/components/App/Main/Techs/Techs.js
@@ -1,9 +1,22 @@
 function Techs() {
-
   return (
-    <>
-      <p>компонент с использованными технологиями.</p>
-    </>
+    <section className="techs">
+      <h2 className="techs__title">Технологии</h2>
+      <h3 className="techs__content-title">7 технологий</h3>
+      <p className="techs__content-text">
+        На курсе веб-разработки мы освоили технологии, которые применили в
+        дипломном проекте.
+      </p>
+      <ul className="techs__description">
+        <li className="techs__description-item">HTML</li>
+        <li className="techs__description-item">CSS</li>
+        <li className="techs__description-item">JS</li>
+        <li className="techs__description-item">React</li>
+        <li className="techs__description-item">Git</li>
+        <li className="techs__description-item">Express.js</li>
+        <li className="techs__description-item">mongoDB</li>
+      </ul>
+    </section>
   );
 }
 
