e0e31082eb00619341a7ed8fea8683cb626c5012
Aleksandr Demidenko <sashatro@mail.ru>
2022-08-09 22:51:56 +0300

    Add profile component template

--- src/components/App/Profile/Profile.js
+++ src/components/App/Profile/Profile.js
@@ -1,6 +1,29 @@
+import { useState, useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
 import Header from "../Header/Header";
+import { CurrentUserContext } from "../../../contexts/CurrentUserContext";
 
 function Profile({ loggedIn, onToggleBurger, isToggleBurger, onSubmit }) {
+  const [name, setName] = useState("Виталий");
+  const [email, setEmail] = useState("pochta@yandex.ru");
+   // Подписка на контекст
+   const currentUser = useContext(CurrentUserContext);
+
+   // После загрузки текущего пользователя из API
+  // его данные будут использованы в управляемых компонентах.
+  useEffect(() => {
+    setName(currentUser.name);
+    setEmail(currentUser.email);
+  }, [currentUser, loggedIn]);
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleEmailChange(e) {
+    setName(e.target.value);
+  }
+
   return (
     <>
       <Header
@@ -9,13 +32,16 @@ function Profile({ loggedIn, onToggleBurger, isToggleBurger, onSubmit }) {
         isToggleBurger={isToggleBurger}
       />
       <form action="#" name="profileform" className="profileform" onSubmit={onSubmit}>
+      <h2 className="profileform__title">{`Привет, ${name}!`}</h2>
       <label htmlFor="nameprofileform" className="profileform__field">Имя</label>
         <input
           id="nameprofileform"
           type="text"
           className="profileform__input"
           name="name"
-          value="Виталий"
+          value={name || "Виталий"}
+          onChange={handleNameChange}
+          readOnly
         />
         <span id="error-nameprofile" className="profileform__input-error"></span>
         <label htmlFor="emailprofileform" className="profileform__field">E-mail</label>
@@ -24,10 +50,15 @@ function Profile({ loggedIn, onToggleBurger, isToggleBurger, onSubmit }) {
           type="email"
           className="profileform__input"
           name="email"
-          value="pochta@yandex.ru"
+          value={email || "pochta@yandex.ru"}
+          onChange={handleEmailChange}
+          readOnly
         />
         <span id="error-emailprofile" className="profileform__input-error"></span>
-        <button className="profileform__submit" type="submit"></button>
+        <span id="error-submitprofile" className="profileform__submit-error">При обновлении профиля произошла ошибка.</span>
+        <button className="profileform__submit" type="submit">Сохранить</button>
+        <button className="profileform__edit" type="submit">Редактировать</button>
+        <Link to="/" className="profileform__exit" type="submit">Выйти из аккаунта</Link>
     </form>
     </>
   );
