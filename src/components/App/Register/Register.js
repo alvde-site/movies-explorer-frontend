00c1aae1e30a9df7e830d85186c930a61677b5e6
Aleksandr Demidenko <sashatro@mail.ru>
2022-08-10 19:56:23 +0300

    Add register component

--- src/components/App/Register/Register.js
+++ src/components/App/Register/Register.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function Register(props) {
   const [name, setName] = useState("");
@@ -32,69 +32,78 @@ function Register(props) {
         onSubmit={handleSubmit}
       >
         <h2 className="auth-form__title">Добро пожаловать!</h2>
-        <fieldset  className="auth-form__field">
-          <label htmlFor="registername" className="auth-form__label">Имя</label>
-          <input
-            id="registerename"
-            type="text"
-            className="auth-form__input auth-form__input_register_name"
-            name="registername"
-            placeholder="Имя"
-            required
-            minLength="2"
-            maxLength="30"
-            value={name || ""}
-            onChange={handleNameChange}
-          />
-          <span
-            id="error-registername"
-            className="auth-form__input-error"
-          ></span>
-        </fieldset>
-
-        <fieldset className="auth-form__field">
-        <label htmlFor="registeremail" className="auth-form__label">E-mail</label>
-          <input
-            id="registeremail"
-            type="email"
-            className="auth-form__input auth-form__input_register_email"
-            name="registeremail"
-            placeholder="Email"
-            required
-            minLength="2"
-            maxLength="30"
-            value={email || ""}
-            onChange={handleEmailChange}
-          />
-          <span
-            id="error-registeremail"
-            className="auth-form__input-error"
-          ></span>
-        </fieldset>
-
-        <fieldset className="auth-form__field">
-        <label htmlFor="registerpassword" className="auth-form__label">Пароль</label>
-          <input
-            id="registerpassword"
-            type="password"
-            className="auth-form__input auth-form__input_register_password"
-            name="link"
-            placeholder="Пароль"
-            required
-            value={password || ""}
-            onChange={handlePasswordChange}
-          />
-          <span
-            id="error-registerpassword"
-            className="auth-form__input-error"
-          ></span>
-        </fieldset>
-        <button className="auth-form__submit" type="submit"></button>
+        <div className="auth-form__inputs">
+          <fieldset className="auth-form__field">
+            <label htmlFor="registername" className="auth-form__label">
+              Имя
+            </label>
+            <input
+              id="registerename"
+              type="text"
+              className="auth-form__input auth-form__input_register_name"
+              name="registername"
+              required
+              minLength="2"
+              maxLength="30"
+              value={name || ""}
+              onChange={handleNameChange}
+            />
+            <span
+              id="error-registername"
+              className="auth-form__input-error"
+            ></span>
+          </fieldset>
+          <fieldset className="auth-form__field">
+            <label htmlFor="registeremail" className="auth-form__label">
+              E-mail
+            </label>
+            <input
+              id="registeremail"
+              type="email"
+              className="auth-form__input auth-form__input_register_email"
+              name="registeremail"
+              required
+              minLength="2"
+              maxLength="30"
+              value={email || ""}
+              onChange={handleEmailChange}
+            />
+            <span
+              id="error-registeremail"
+              className="auth-form__input-error"
+            ></span>
+          </fieldset>
+          <fieldset className="auth-form__field">
+            <label htmlFor="registerpassword" className="auth-form__label">
+              Пароль
+            </label>
+            <input
+              id="registerpassword"
+              type="password"
+              className="auth-form__input auth-form__input_type_error auth-form__input_register_password"
+              name="link"
+              required
+              value={password || ""}
+              onChange={handlePasswordChange}
+            />
+            <span
+              id="error-registerpassword"
+              className="auth-form__input-error"
+            >
+              Что-то пошло не так...
+            </span>
+          </fieldset>
+        </div>
+        <button className="auth-form__submit" type="submit">
+          Зарегистрироваться
+        </button>
         <div className="auth-form__signin">
-          <p className="auth-form__question">Уже зарегистрированы?</p>
-          <Link to="/signin" className="auth-form__login-link">
-            Войти
-          </Link>
+          <div className="auth-form__redirect">
+            <p className="auth-form__question">Уже зарегистрированы?</p>
+            <Link to="/signin" className="auth-form__login-link">
+              Войти
+            </Link>
+          </div>
         </div>
       </form>
     </div>
