ef5692f83a6668d42f55eb7a93caf98c00fc4f77
Aleksandr Demidenko <sashatro@mail.ru>
2022-08-11 00:54:14 +0300

    Удалить прелоадер из Movies

--- src/components/App/Movies/Movies.js
+++ src/components/App/Movies/Movies.js
@@ -1,5 +1,4 @@
 import SearchForm from "./SearchForm/SearchForm";
-import Preloader from "../Preloader/Preloader";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import Header from "../Header/Header";
 import Navigation from "../Navigation/Navigation";
@@ -38,7 +37,6 @@ function Movies({
       </main>
       <Footer />
       <Navigation isToggleBurger={isToggleBurger} />
-      <Preloader />
     </>
   );
 }
