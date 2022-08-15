0e289146eebc6f36173028722a65ae4a319caa26
Aleksandr Demidenko <sashatro@mail.ru>
2022-08-15 13:12:28 +0300

    feat: Add routes to MainApi

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,3 +1,29 @@
+
+  //   addLike(cardId) {
+  //     return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+  //       method: "PUT",
+  //       headers: this._headers,
+  //       credentials: 'include',
+  //     }).then(this._checkResponse);
+  //   }
+
+  //   removeLike(cardId) {
+  //     return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+  //       method: "DELETE",
+  //       headers: this._headers,
+  //       credentials: 'include',
+  //     }).then(this._checkResponse);
+  //   }
+
+  //   changeLikeCardStatus(cardId, notLiked) {
+  //     if (notLiked) {
+  //       return this.addLike(cardId);
+  //     } else {
+  //       return this.removeLike(cardId);
+  //     }
+  //   }
+
+
 class MainApi {
   constructor({ baseUrl, headers }) {
     this._baseUrl = baseUrl;
@@ -13,25 +39,6 @@ class MainApi {
       credentials: "include",
     }).then(this._checkResponse);
   }
-
-  //   getUserInfo() {
-  //     return fetch(`${this._baseUrl}/users/me`, {
-  //       headers: this._headers,
-  //       credentials: 'include',
-  //     }).then(this._checkResponse);
-  //   }
-
-  //   editUserInfo({ name, about }) {
-  //     return fetch(`${this._baseUrl}/users/me`, {
-  //       method: "PATCH",
-  //       headers: this._headers,
-  //       body: JSON.stringify({
-  //         name: name,
-  //         about: about,
-  //       }),
-  //       credentials: 'include',
-  //     }).then(this._checkResponse);
-  //   }
 
   createMovie({
     country,
@@ -67,51 +74,73 @@ class MainApi {
   }
 
   deleteMovie(movieId) {
-      return fetch(`${this._baseUrl}/cards/${movieId}`, {
-        method: "DELETE",
-        headers: this._headers,
-        credentials: 'include',
-      }).then(this._checkResponse);
-    }
+    return fetch(`${this._baseUrl}/cards/${movieId}`, {
+      method: "DELETE",
+      headers: this._headers,
+      credentials: "include",
+    }).then(this._checkResponse);
+  }
 
-  //   addLike(cardId) {
-  //     return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
-  //       method: "PUT",
-  //       headers: this._headers,
-  //       credentials: 'include',
-  //     }).then(this._checkResponse);
-  //   }
+  getCurrentUser() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+      credentials: "include",
+    }).then(this._checkResponse);
+  }
 
-  //   removeLike(cardId) {
-  //     return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
-  //       method: "DELETE",
-  //       headers: this._headers,
-  //       credentials: 'include',
-  //     }).then(this._checkResponse);
-  //   }
+  updateUser({ name, email }) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      credentials: "include",
+      body: JSON.stringify({
+        name,
+        email,
+      }),
+    }).then(this._checkResponse);
+  }
 
-  //   changeLikeCardStatus(cardId, notLiked) {
-  //     if (notLiked) {
-  //       return this.addLike(cardId);
-  //     } else {
-  //       return this.removeLike(cardId);
-  //     }
-  //   }
+  register({ name, password, email }) {
+    return fetch(`${this._baseUrl}/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        name,
+        password,
+        email,
+      }),
+    }).then(this._checkResponse);
+  }
 
-  //   editAvatarInfo({ avatar }) {
-  //     return fetch(`${this._baseUrl}/users/me/avatar`, {
-  //       method: "PATCH",
-  //       headers: this._headers,
-  //       credentials: 'include',
-  //       body: JSON.stringify({
-  //         avatar: avatar,
-  //       }),
-  //     }).then(this._checkResponse);
-  //   }
+  login({ password, email }) {
+    return fetch(`${this._baseUrl}/signin`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        password,
+        email,
+      }),
+    }).then((res) => {
+      return res.json();
+    });
+  }
 
-    _checkResponse(res) {
-      return res.ok ? res.json() : Promise.reject(`Ошибка ${res.status}`);
-    }
+  signout() {
+    return fetch(`${this._baseUrl}/signout`, {
+      headers: this._headers,
+      credentials: "include",
+    }).then(this._checkResponse);
+  }
+
+  _checkResponse(res) {
+    return res.ok ? res.json() : Promise.reject(`Ошибка ${res.status}`);
+  }
 }
 
 export const MainApiSet = new MainApi({
